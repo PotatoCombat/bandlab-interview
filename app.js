afc8d9931671c749349131838311d4a833e930b2
PotatoCombat <39809238+PotatoCombat@users.noreply.github.com>
2022-07-09 17:07:10 +0800

    Add Page 1 template

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 
+const indexRouter = require('./routes/posts');
+
 const app = express();
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/posts', indexRouter);
 
 module.exports = app;
